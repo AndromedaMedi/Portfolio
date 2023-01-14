7e803854fc28e9fb65168769b49679434c4b2a7e
AndromedaMedi <88595811+AndromedaMedi@users.noreply.github.com>
2023-01-14 00:43:25 +0000

    fixed project link

--- src/components/Project1.js
+++ src/components/Project1.js
@@ -13,8 +13,8 @@ const Project1 = () => {
             <h3>Solo Project</h3>
             <h3 className='project-type'>Python  |  Flask  |  PSQL  |  CSS</h3>
             <h5>Completed in 7 days</h5>
-            
-            <span className='icon'><FaGithubSquare size={50}/><a className='link' href="https://github.com/AndromedaMedi/week_5_solo_project.git"></a></span>
+        
+            <a href="https://github.com/AndromedaMedi/week_5_solo_project.git"><span className='icon'><FaGithubSquare size={50}/></span></a>
         
         </div>
     )
