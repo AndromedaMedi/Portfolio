33a264c5949247fba41f3002d05d9ed68f3b512b
AndromedaMedi <88595811+AndromedaMedi@users.noreply.github.com>
2023-01-16 19:40:18 +0000

    small button change

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import {GoMarkGithub} from 'react-icons/go'
-import {GrLinkedin} from 'react-icons/gr'
+import {BsLinkedin} from 'react-icons/bs'
 import {FaGithubSquare} from 'react-icons/fa'
 
 const Contact = () => {
@@ -11,7 +11,7 @@ const Contact = () => {
 
             <h3 className='contact-msg'>Please Follow the Below Links to Contact Me or View More of My Work</h3>
             <div className='icons'>
-                <span className='icon'><GrLinkedin size={50}/></span>
+                <span className='icon'><BsLinkedin size={50}/></span>
                 <a href='https://github.com/AndromedaMedi?tab=repositories'>
                 <span className='icon'><FaGithubSquare size={55}/></span>
                 </a>
