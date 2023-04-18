28b461c82db02c76da6b933999336e595671c0a0
AndromedaMedi <88595811+AndromedaMedi@users.noreply.github.com>
2023-04-18 21:57:14 +0300

    fixed the homepage name issue

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,11 +12,13 @@ const Home = () => {
         <div className='home-page'>
             <div className='name-title'>
                 <div className='vertical-line'></div>
-                <div className='title'>
-                    <h1>Andromeda Akoulli</h1>
-                    <h2>Software Developer</h2>
+                <div className='name-title-detail'>
+                    <h1 className='name'>Andromeda Akoulli</h1>
+                    <h2 className='title'>Software Developer</h2>
                 </div>
-                <h2 className='welcome'>Welcome to my portfolio</h2>
+                <div className='welcome'>
+                    <h2 className='welcome-msg'>Welcome to my portfolio</h2>
+                </div>
             </div>
             <img className='pic' src={profilePic} alt='profile-pic' onError={onImageError}/>
         </div>
