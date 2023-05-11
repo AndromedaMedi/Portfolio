f44cf1ba05fa15710217794967eeef321c5c59a6
AndromedaMedi <88595811+AndromedaMedi@users.noreply.github.com>
2023-05-11 21:11:25 +0100

    Updates

---
+++ precache-manifest.204ff87f9344b5e29080f2b5c6dff07a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "15af3ccb63670f69a6fb",
+    "url": "/portfolio/static/css/main.ca4d562e.chunk.css"
+  },
+  {
+    "revision": "15af3ccb63670f69a6fb",
+    "url": "/portfolio/static/js/main.15af3ccb.chunk.js"
+  },
+  {
+    "revision": "a89266deb73d87b01859",
+    "url": "/portfolio/static/js/1.a89266de.chunk.js"
+  },
+  {
+    "revision": "ab11afb2f4375574e4b9",
+    "url": "/portfolio/static/js/2.ab11afb2.chunk.js"
+  },
+  {
+    "revision": "f8b95647bec3d732df71",
+    "url": "/portfolio/static/js/runtime~main.f8b95647.js"
+  },
+  {
+    "revision": "7380a3ccf0d3b315a20c2e1df0765680",
+    "url": "/portfolio/static/media/1_home_page.7380a3cc.png"
+  },
+  {
+    "revision": "ed0342b730d060551497284757f39f46",
+    "url": "/portfolio/static/media/profile_pic.ed0342b7.png"
+  },
+  {
+    "revision": "d8e95dfd28061fe9ac81eaa775895995",
+    "url": "/portfolio/static/media/unread-recording.d8e95dfd.mp4"
+  },
+  {
+    "revision": "a60c34b35994d20e2ff6139e32897d12",
+    "url": "/portfolio/static/media/rise-demo.a60c34b3.mov"
+  },
+  {
+    "revision": "64cebb327b41ac5e918be0147c0bdfaa",
+    "url": "/portfolio/static/media/discover_page.64cebb32.png"
+  },
+  {
+    "revision": "82058a1d7567cc964f920b0290240f3f",
+    "url": "/portfolio/static/media/logo_no_background.82058a1d.png"
+  },
+  {
+    "revision": "13d1433ba12e2656deb66d03e362e5f0",
+    "url": "/portfolio/index.html"
+  }
+];
